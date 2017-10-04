79820fae37192bfc3ca9d881f6ea17cc37953b3d
klumb <klumbard@unomaha.edu>
2017-10-04 16:03:24 -0500

    added home view

--- volunteer_activity/views.py
+++ volunteer_activity/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.utils import timezone
+from .models import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.db.models import Sum
 
-# Create your views here.
+
+def home(request):
+   return render(request, 'volunteer_activity/home.html',
+                 {'volunteer_activity': home})
+
