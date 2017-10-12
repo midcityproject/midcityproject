6890541ffbd53dc3219573081513d9b1f5baf908
vjampani <vjampnai@unomaha.edu>
2017-10-12 00:16:55 -0500

    update

--- volunteer_activity/views.py
+++ volunteer_activity/views.py
@@ -1,14 +1,38 @@
-from django.conf.urls import url
-from . import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.utils import timezone
+from .models import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework import status
 
 
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^$', views.aboutus, name='aboutus'),
-    url(r'^$', views.contactus, name='contactus'),
-    
 
 
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+def home(request):
+   return render(request, 'volunteer_activity/home.html',
+                 {'home': home})
+
+def aboutus(request):
+   return render(request, 'volunteer_activity/aboutus.html',
+                 {'aboutus': aboutus})
+
+def activity(request):
+   return render(request, 'volunteer_activity/events.html',
+                 {'events': events})
+
+def tracking(request):
+   return render(request, 'volunteer_activity/tracking.html',
+                 {'trackingy': tracking})
+
+
+def contactus(request):
+   return render(request, 'volunteer_activity/contactus.html',
+                 {'contactus': contactus})
+
+
+
+
+
+#
