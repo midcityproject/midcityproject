e641006c3d1f932a8e3083b2dc0276ddcb48bd3b
vaishnavisuguru <vsuguru@unomaha.edu>
2017-11-13 10:21:27 -0600

    register page

--- volunteer_activity/views.py
+++ volunteer_activity/views.py
@@ -23,8 +23,24 @@ def manage_activity(request):
                  {'volunteer_activity': manage_activity, 'users': users,'userevents': userevents, })
 
 def register(request):
-   return render(request, 'volunteer_activity/register.html',
-                 {'volunteer_activity': register})
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            # Create the user profile
+            profile = Profile.objects.create(user=new_user)
+            return render(request,
+                          'volunteer_activity/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'volunteer_activity/register.html', {'user_form': user_form})
 
 def contactus(request):
    return render(request, 'volunteer_activity/contactus.html',
