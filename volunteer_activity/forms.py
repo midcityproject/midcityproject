d9a723a85c6fa98b7fc33e2b84f507042fd03ca4
jampanivijaysai <jampanivijaysai2@gmail.com>
2017-11-19 00:30:27 -0800

    Styling updated
    Updated styling for
    signin, register, volunteer edit, volunteer add, event add and event edit pages

--- volunteer_activity/forms.py
+++ volunteer_activity/forms.py
@@ -43,7 +43,7 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
-		
+
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
